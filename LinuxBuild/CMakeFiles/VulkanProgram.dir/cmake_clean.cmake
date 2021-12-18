f0621a5e050e693642a331163a55e9b11e746574
Jiaxiong Jiao <jiaxiongjiao@icloud.com>
2021-12-18 01:16:12 -0500

    Added built binary for linux. In case cmake is not configured correctly on other linux machine.

---
+++ LinuxBuild/CMakeFiles/VulkanProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanProgram.dir/src/main.cpp.o"
+  "VulkanProgram"
+  "VulkanProgram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/context.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/egl_context.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/glx_context.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/init.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/input.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/linux_joystick.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/monitor.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/null_init.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/null_init.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/null_joystick.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/null_joystick.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/null_monitor.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/null_monitor.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/null_window.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/null_window.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/osmesa_context.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/platform.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/platform.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/posix_module.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/posix_module.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/posix_thread.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/posix_time.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/vulkan.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/window.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/x11_init.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/x11_monitor.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/x11_window.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/jiaxiongjiao/VulkanTriangle/glfw3/src/xkb_unicode.c" "/home/jiaxiongjiao/VulkanTriangle/LinuxBuild/glfw3/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_DEFAULT_SOURCE"
+  "_GLFW_X11"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../VulkanSDK/source/Vulkan-Headers/include"
+  "../glfw3/include"
+  "../glfw3/src"
+  "glfw3/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
