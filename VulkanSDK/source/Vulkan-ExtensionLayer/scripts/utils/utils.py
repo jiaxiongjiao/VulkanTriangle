d4719201be37604261c78cd838268a13290182b4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 04:25:52 +0000

    Adding numeric_owner as keyword arguement

--- VulkanSDK/source/Vulkan-ExtensionLayer/scripts/utils/utils.py
+++ VulkanSDK/source/Vulkan-ExtensionLayer/scripts/utils/utils.py
@@ -88,7 +88,7 @@ def expand_archive(path):
                                                         if not is_within_directory(path, member_path):
                                                             raise Exception("Attempted Path Traversal in Tar File")
                                                 
-                                                    tar.extractall(path, members, numeric_owner) 
+                                                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                                                     
                                                 
                                                 safe_extract(tar)
